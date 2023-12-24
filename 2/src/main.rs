b0711ac1caea26d5d827e16c3370ef6b474c8cb5
Glenn <glenn.reumers@appwise.be>
2023-12-24 14:25:21 +0100

    Create parse_file function

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -17,60 +17,60 @@ enum CubeColor {
 }
 
 fn main() {
-    let game_struct = parse_game();
-
-    const MAX_RED_CUBES: u32 = 12;
-    const MAX_GREEN_CUBES: u32 = 13;
-    const MAX_BLUE_CUBES: u32 = 14;
-
     let file = fs::read("./src/input.txt").unwrap();
+    let games = parse_file(file);
+
+    games.iter().for_each(|game| {
+        println!("{}", game.id)
+    });
+
+}
+
+fn parse_file(file: Vec<u8>) -> Vec<Game> {
     let lines = file.lines();
+    let mut games: Vec<Game> = vec![];
 
     lines.for_each(|line| {
         let result = line.unwrap();
-        let mut game_is_possible = true;
+        let parts: Vec<_> = result.split(':').collect();
+        let game_meta = parts[0].trim();
+        let game_meta_parts: Vec<_> = game_meta.split(' ').collect();
+        let game_id: u32 = game_meta_parts[1].parse().unwrap();
 
-        let parts: Vec<_> = result.split(':').into_iter().collect();
-        let game_title = parts[0];
+        let game_result = parts[1].trim();
+        let game_set_lines: Vec<_> = game_result.split(";").collect();
+        let mut cubes: Vec<Cube> = vec![];
 
-        println!("{}", game_title);
+        for game_set_line in game_set_lines {
+            let cube_lines: Vec<_> = game_set_line.split(',').collect();
+            let mut set_cubes: Vec<Cube> = vec![];
 
-        let game = parts[1];
-        let game_sets: Vec<_> = game.split(";").collect();
+            for cube_line in cube_lines  {
+                let cube_line_parts: Vec<_> = cube_line.trim().split(' ').collect();
+                let cube_line_amount = cube_line_parts[0];
+                let cube_line_color = cube_line_parts[1];
 
-        for set in game_sets {
-            let cube_colors: Vec<_> = set.split(',').collect();
-            
-            for cube_color in cube_colors {
-                let cube_color_parts: Vec<_> = cube_color.split(' ').collect();
-                let amount: u32 = cube_color_parts[1].parse().unwrap();
-                let color = cube_color_parts[2];
+                let cube = Cube {
+                    amount: cube_line_amount.parse::<u32>().unwrap(),
+                    color: CubeColor::RED
+                };
 
-                if amount > MAX_BLUE_CUBES {
-                    game_is_possible = false
-                }
+                set_cubes.push(cube);
+                            }
 
-                // println!("{}", color)
+            for cube in set_cubes {
+                cubes.push(cube);
             }
-            // println!("{}", set)
+
         }
+    
+        let game = Game {
+            id: game_id,
+            sets: cubes
+        };
 
-        println!("{}", game_is_possible);
+        games.push(game)
     });
-}
 
-fn parse_game() -> Game {
-    let game = Game {
-        id: 0,
-        sets: vec![Cube {
-            color: CubeColor::RED,
-            amount: 10
-        }, 
-        Cube {
-            color: CubeColor::BLUE,
-            amount: 5
-        }]
-    };
-
-    game
+    return games
 }
