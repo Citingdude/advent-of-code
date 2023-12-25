a1a22c7fed15591abe0d6ecd647adf491a01a9e1
Glenn <glenn.reumers@appwise.be>
2023-12-25 11:35:46 +0100

    Finish day 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::BufRead};
+use std::{fs, io::BufRead, fmt};
 
 struct Game {
     id: u32,
@@ -16,13 +16,34 @@ enum CubeColor {
     BLUE
 }
 
+impl fmt::Display for CubeColor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match self {
+            CubeColor::BLUE => write!(f, "Blue"),
+            CubeColor::RED => write!(f, "Red"),
+            CubeColor::GREEN => write!(f, "Green"),
+       }
+    }
+}
+
+const MAX_RED_CUBES: u32 = 12;
+const MAX_GREEN_CUBES: u32 = 13;
+const MAX_BLUE_CUBES: u32 = 14;
+
 fn main() {
     let file = fs::read("./src/input.txt").unwrap();
     let games = parse_file(file);
+    let mut valid_games: Vec<u32> = vec![];
 
-    games.iter().for_each(|game| {
-        println!("{}", game.id)
+    games.iter().for_each(|game: &Game| {
+        if validate_game(game) {
+            valid_games.push(game.id)
+        }
     });
+
+    let valid_games_total: u32 = valid_games.iter().sum();
+
+    println!("{}", valid_games_total)
 
 }
 
@@ -52,7 +73,7 @@ fn parse_file(file: Vec<u8>) -> Vec<Game> {
 
                 let cube = Cube {
                     amount: cube_line_amount.parse::<u32>().unwrap(),
-                    color: CubeColor::RED
+                    color: get_cube_color(cube_line_color)
                 };
 
                 set_cubes.push(cube);
@@ -73,4 +94,50 @@ fn parse_file(file: Vec<u8>) -> Vec<Game> {
     });
 
     return games
+}
+
+fn validate_game(game: &Game) -> bool {
+    let mut is_valid = true;
+
+    for cube in &game.sets {
+        match cube.color {
+            CubeColor::RED => {
+                let valid = validate_cube(cube, MAX_RED_CUBES);
+                if valid == false {
+                    is_valid = false
+                } 
+            },
+            CubeColor::BLUE => {
+                let valid = validate_cube(cube, MAX_BLUE_CUBES);
+                if valid == false {
+                    is_valid = false
+                }
+            },
+            CubeColor::GREEN => {
+                let valid = validate_cube(cube, MAX_GREEN_CUBES);
+                if valid == false {
+                    is_valid = false
+                }
+            }
+        }
+    }
+    
+    is_valid
+}
+
+fn validate_cube(cube: &Cube, max: u32) -> bool {
+    if cube.amount > max {
+        return false
+    } else {
+        return true
+    }
+}
+
+fn get_cube_color(color: &str) -> CubeColor {
+    match color {
+        "red" => CubeColor::RED,
+        "blue" => CubeColor::BLUE,
+        "green" => CubeColor::GREEN,
+        &_ => CubeColor::BLUE,
+    }
 }
